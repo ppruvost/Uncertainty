45cab83f53f6cd5c34c58b41ed05cdfcde6ec8df
ppruvost <patrick.pruvost50@gmail.com>
2026-04-16 16:33:49 +0200

    Update analyse_erreurs.js

--- modules/analyse_erreurs.js
+++ modules/analyse_erreurs.js
@@ -5,11 +5,11 @@ function genererAnalyse(U, I) {
   texte += "<li>📌 Variations possibles de la tension : mauvais contact, alimentation instable</li>";
   texte += "<li>📌 Variations du courant : résistance interne des fils, échauffement</li>";
 
-  if (U.incertitude > 0.5) {
+  if (U.incertitudeA > 0.5) {
     texte += "<li>⚠️ Incertitude tension élevée → vérifier le voltmètre</li>";
   }
 
-  if (I.incertitude > 0.05) {
+  if (I.incertitudeA > 0.05) {
     texte += "<li>⚠️ Incertitude courant élevée → vérifier le calibre de l’ampèremètre</li>";
   }
 
